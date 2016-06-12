d1c2bee25e1b3f099901393efb4759b28852dd6f
Nicolas Artman <nicolasartman@gmail.com>
2016-06-12 05:01:35 -0700

    Fix chapter page loading logic; improve focused item shadow

--- src/js/chapter.js
+++ src/js/chapter.js
@@ -13,65 +13,71 @@ import chapters from './chapter-data.js';
 const Chapter = React.createClass({
   getInitialState: () => ({}),
   componentDidMount: function() {
-    console.log('mounted chapter component');
+    console.log('mounted chapter component', this.props.params.id);
     this.setState({
-      chapterId: this.props.params.id
+      chapterId: parseInt(this.props.params.id, 10)
     });
-    
-    const learningItems = [...ReactDom.findDOMNode(this).querySelectorAll('.learning-item')]
-      .map(item => ({
-        top: item.offsetTop,
-        node: item
-      }));
-    
-    let recentlyScrolled = false;
-    let focusedLearningItem = null;
-    
-    this.scrollListener = () => recentlyScrolled = true;
-    
-    window.addEventListener('scroll', this.scrollListener);
-    
-    function updateFocusedLearningItem() {
-      recentlyScrolled = false;
-      const scrollTop = window.pageYOffset;
-      
-      let focusedItem = null;
-      let index = 0;
-      while (focusedItem === null && index < learningItems.length) {
-        // If the item is the topmost one in the page (with a UX allowance
-        // so part of the item can be scrolled past while still considering
-        // it in focus)
-        if (learningItems[index].top > scrollTop - 150) {
-          focusedItem = learningItems[index];
-        } else {
-          index += 1;            
+
+    // ultra hack. needs to be refactored majorly.
+    window.setTimeout(() => {      
+      const learningItems = [...ReactDom.findDOMNode(this).querySelectorAll('.learning-item')]
+        .map(item => ({
+          top: item.offsetTop,
+          node: item
+        }));
+
+      let recentlyScrolled = false;
+      let focusedLearningItem = null;
+
+      this.scrollListener = () => recentlyScrolled = true;
+
+      window.addEventListener('scroll', this.scrollListener);
+
+      function updateFocusedLearningItem() {
+        recentlyScrolled = false;
+        const scrollTop = window.pageYOffset;
+
+        let focusedItem = null;
+        let index = 0;
+        while (focusedItem === null && index < learningItems.length) {
+          // If the item is the topmost one in the page (with a UX allowance
+          // so part of the item can be scrolled past while still considering
+          // it in focus)
+          if (learningItems[index].top > scrollTop - 150) {
+            focusedItem = learningItems[index];
+          } else {
+            index += 1;
+          }
+        }
+
+        if (focusedItem && focusedLearningItem !== focusedItem) {
+          focusedLearningItem = focusedItem;
+          learningItems.map(item => item.node.classList.remove('focused'));
+          focusedLearningItem.node.classList.add('focused');
         }
       }
-      
-      if (focusedItem && focusedLearningItem !== focusedItem) {
-        focusedLearningItem = focusedItem;
-        learningItems.map(item => item.node.classList.remove('focused'));
-        focusedLearningItem.node.classList.add('focused');
-      }
-    }
-    
-    this.scrollCheckInterval = window.setInterval(() => {
-      if (recentlyScrolled) {
-       updateFocusedLearningItem()
-      }
-    }, 100);
-    
-    updateFocusedLearningItem();
+
+      this.scrollCheckInterval = window.setInterval(() => {
+        if (recentlyScrolled) {
+         updateFocusedLearningItem()
+        }
+      }, 100);
+
+      updateFocusedLearningItem();
+    }, 1000)
   },
   componentWillUnmount: function () {
     window.clearInterval(this.scrollCheckInterval);
     window.removeEventListener('scroll', this.scrollListener);
   },
   render: function() {
+    console.log('huh??', this.state.chapterId);
+
     // Use the magnets chapter if there isn't existing dummy data for the given one
-    const currentChapter = chapters[this.state.chapterId] || chapters[13];
-    
-    return (
+    const currentChapter = chapters.find(chapter => chapter.number === this.state.chapterId);
+    console.log('currentChapter', currentChapter);
+
+    return currentChapter ? (
       <div>
         <main id="main" className="container chapter">
           <div className="pure-g">
@@ -83,7 +89,7 @@ const Chapter = React.createClass({
           </div>
         </main>
       </div>
-    );
+    ) : null;
   }
 });
 
