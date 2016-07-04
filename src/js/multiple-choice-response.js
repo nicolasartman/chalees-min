c9fdd6ef34eede43ad36f79f8a746d91e358880a
Nicolas Artman <nicolasartman@gmail.com>
2016-07-04 01:09:28 -0700

    Fix toggling behavior of multiple choice responses

--- src/js/multiple-choice-response.js
+++ src/js/multiple-choice-response.js
@@ -19,8 +19,8 @@ const MultipleChoiceResponse = React.createClass({
     let selectedChoices = this.props.response || [];
     
     // Deselection
-    if (selectedChoices.find(aChoice => aChoice.id === choice.id)) {
-      selectedChoices = selectedChoices.filter(aChoice => achoice.id !== choice.id);
+    if (selectedChoices.find(selectedChoice => selectedChoice.id === choice.id)) {
+      selectedChoices = selectedChoices.filter(selectedChoice => selectedChoice.id !== choice.id);
     }
     // Selection
     else {
@@ -37,19 +37,23 @@ const MultipleChoiceResponse = React.createClass({
     });
   },
   isChoiceSelected(choice) {
-    return this.props.response && this.props.response.find(aChoice => aChoice.id === choice.id);
+    return this.props.response && this.props.response.find(
+      selectedChoice => selectedChoice.id === choice.id);
   },
   updateFeedback() {
+    const feedbacks = this.props.automaticResponses || [];
     const selectedChoices = this.props.response || [];
     const selectedChoiceIds = selectedChoices.map(choice => choice.id);
     
     // Check each automatic response to see if the currently selected choices
     // match any answer set provided. Matching is exact set match, first one found.
-    const {response: feedback} = this.props.automaticResponses.find((item) => (
-      intersection(selectedChoiceIds, item.answerSet).length === item.answerSet.length
-    ));
+    const feedbackItem = feedbacks.find((item) => {
+      const intersectionSetSize = intersection(selectedChoiceIds, item.answerSet).length;
+      return intersectionSetSize === selectedChoiceIds.length &&
+        intersectionSetSize === item.answerSet.length;
+    });
     
-    this.setState({feedback});
+    this.setState({feedback: feedbackItem ? feedbackItem.response : null});
   },
   render() {
     return (
@@ -57,7 +61,7 @@ const MultipleChoiceResponse = React.createClass({
         <div className="choices">
           {this.props.choices.map((choice) => (
             <button key={choice.id}
-                    disabled={this.state.maxMultiselectionReached && !this.isChoiceSelected(choice.id)}
+                    disabled={this.state.maxMultiselectionReached && !this.isChoiceSelected(choice)}
                     className={'choice' + (this.isChoiceSelected(choice) ? ' selected' : '')}
                     onClick={() => this.toggleChoice(choice)}>
               {choice.text}
