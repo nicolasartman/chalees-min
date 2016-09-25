0d82b3f47f48fef7e5f75f43d2636d563ba837f7
Nicolas Artman <nicolasartman@gmail.com>
2016-09-25 15:34:39 -0700

    Stop upscaling image type learning item images

--- src/js/image-content.js
+++ src/js/image-content.js
@@ -1,3 +1,3 @@
 export default (props) => (
-  <img className="pure-img" src={`https://chalees-min.imgix.net${props.imagePath}?w=726&fit=clip&auto=format,compress`} />
+  <img className="pure-img" src={`https://chalees-min.imgix.net${props.imagePath}?w=726&fit=max&auto=format,compress`} />
 )
