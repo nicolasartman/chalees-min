c0bc47c6c8f79610ae32db0b6656bd1309697513
Nicolas Artman <nicolasartman@gmail.com>
2017-05-19 21:47:17 -0700

    Update live class button text to note it's today

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -48,7 +48,7 @@ const HomePage = React.createClass({
           </div>
           {<a href="https://hangouts.google.com/hangouts/_/fwzxlibgxvchxcz4hlcnjie4due" className={"live-banner " + (this.state.grade6 || this.state.grade7 ? 'live-banner-invisible' : '')}>
             <img src={cameraIcon} style={{position: 'relative', top: 3, marginRight: 7}}/>
-            Live Class, Every Saturday at 12PM
+            Live Class Today at 12PM!
           </a>}
         </div>      
         
