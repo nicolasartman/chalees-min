8279393171bf992bc2846c30503f3831c9aaeff9
Kunal Chawla <chawlak@gmail.com>
2017-11-14 13:36:51 +0530

    New English chapter on Parleg

--- src/js/chapter-data/englishData.js
+++ src/js/chapter-data/englishData.js
@@ -1,4 +1,260 @@
 const englishData = [{
+  id: 'parleg',
+  thumbnailImagePath: '/chapter-thumbnails/english/parleg.jpeg',
+  title: 'Parle-G',
+  caption: 'Learn English with Parle-G',
+  categoryId: 'food',
+  description: 'Learn English with Parle-G',
+  items: [{
+    id: 'parle-g-video',
+    kind: 'video',
+    time: 5,
+    title: 'Parle-G Video (in Hindi)',
+    instructions: dedent`
+    The packet of Parle-G says: _World\'s largest selling biscuit._ 
+
+    Let us learn about these words.`,
+    videoId: '-9VOabKD3wo',
+  }, {
+    id: 'parleg-match-picture-game',
+    kind: 'imageSelect',
+    time: 2,
+    title: 'Game: Match Words With Picture',
+    questions: [{
+      word: 'Largest state in India',
+      images: [{
+        id: 1,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/world-from-space.jpeg',
+      }, {
+        id: 2,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/largest-state-india-up.jpeg'
+      }, {
+        id: 3,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/apostrophe.jpeg'
+      }, {
+        id: 4,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/genius-chess.jpeg'
+      }],
+      answer: 2
+    }, {
+      word: 'Our world from space',
+      images: [{
+        id: 1,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/world-from-space.jpeg',
+      }, {
+        id: 2,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/largest-state-india-up.jpeg'
+      }, {
+        id: 3,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/apostrophe.jpeg'
+      }, {
+        id: 4,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/genius-chess.jpeg'
+      }],
+      answer: 1
+    }, {
+      word: 'Genius',
+      images: [{
+        id: 1,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/world-from-space.jpeg',
+      }, {
+        id: 2,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/largest-state-india-up.jpeg'
+      }, {
+        id: 3,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/apostrophe.jpeg'
+      }, {
+        id: 4,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/genius-chess.jpeg'
+      }],
+      answer: 4
+    }, {
+      word: 'Childrens day needs an _____',
+      images: [{
+        id: 1,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/world-from-space.jpeg',
+      }, {
+        id: 2,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/largest-state-india-up.jpeg'
+      }, {
+        id: 3,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/apostrophe.jpeg'
+      }, {
+        id: 4,
+        path: '//s3-ap-southeast-1.amazonaws.com/chalees-min-images/learning-items/english/genius-chess.jpeg'
+      }],
+      answer: 3
+    }]
+  }, {
+    id: 'computer-timed-mcq',
+    kind: 'multipleChoiceResponse',
+    time: 1,
+    isTimed: 30,
+    maxSelected: 1,
+    presenterImagePath: '/instructors/kunal2.jpg',
+    title: 'Fill In The Blank',
+    instructions: dedent`
+      My ______ computer is not working well.
+    `,
+    choices: [
+      {
+        id: 'opt1',
+        text: 'sister'
+      },
+      {
+        id: 'opt2',
+        text: 'sisters'
+      },
+      {
+        id: 'opt3',
+        text: 'sister\'s'
+      },
+      {
+        id: 'opt4',
+        text: 'sisters\''
+      }
+    ],
+    automaticResponses: [
+      {
+        answerSet: ['opt1',],
+        response: '__Wrong__. Since the computer belongs to my sister (मेरी बहन का कंप्यूटर) we need an apostrophe. The correct answer is sister\'s.'
+      },
+      {
+        answerSet: ['opt2'],
+        response: '__Incorrect__. The computer belongs to my sister(मेरी बहन का कंप्यूटर). Thus we need an apostrophe. The correct answer is sister\'s.'
+      },
+      {
+        answerSet: ['opt3'],
+        response: '__Correct__. Since the computer belongs to my sister(मेरी बहन का कंप्यूटर), sister\'s is correct.'
+      },
+      {
+        answerSet: ['opt4'],
+        response: '__Wrong__. Sisters\' means that the computer belongs to not one but two or more sisters (बहनों का कंप्यूटर).'
+      }
+    ]
+  }, {
+    id: 'sells-quiz',
+    kind: 'multipleChoiceResponse',
+    time: 1,
+    title: 'Fill In The Blank (Again)',
+    presenterImagePath: '/instructors/kunal2.jpg',
+    instructions: dedent`
+    Parle-G sells biscuits. Bollywood sells _____.`,
+    maxSelected: 1,
+    choices: [
+      {
+        id: 'opt1',
+        text: 'potato chips'
+      },
+      {
+        id: 'opt2',
+        text: 'tea'
+      },
+      {
+        id: 'opt3',
+        text: 'dreams'
+      },
+      {
+        id: 'opt4',
+        text: 'clothes'
+      }
+    ],
+    automaticResponses: [
+      {
+        answerSet: ['opt1',],
+        response: 'To sell something means to give that thing in exchange for money. Bollywood does not sell potato chips.'
+      },
+      {
+        answerSet: ['opt2'],
+        response: '__Incorrect.__ Bollywood does not sell tea. To sell something means to give that thing in exchange for money.'
+      },
+      {
+        answerSet: ['opt3'],
+        response: '__Correct__. To sell something means to give that thing in exchange for money. When we go to see a movie, we pay money, and receive entertainment or dreams in return.'
+      },
+      {
+        answerSet: ['opt4'],
+        response: 'To sell something means to give that thing in exchange for money. Bollywood does not sell clothes.'
+      }
+    ]
+  }, {
+    id: 'apostrophe-short-answer',
+    kind: 'textResponse',
+    time: 3,
+    short: true,
+    title: 'Teach A Friend',
+    presenterImagePath: '/instructors/kunal2.jpg',
+    instructions: dedent`
+      How would you explain __apostrophe__ to a friend?
+      
+      Write your answer in the box below.`,
+    hacks: {
+      afterBody: {
+        preface: 'Here are some examples from other students:',
+        show: 'locked',
+        fakePeerResponses: [
+          {
+            studentName: 'Prabsimar',
+            imagePath: '/fake-responses/prabsimar.png',
+            response: 'I change the english to hindi, and if there is a का, के, की then I use the apostrophe.'
+          },
+          {
+            studentName: 'Koushiki',
+            imagePath: '/fake-responses/koushiki.png',
+            response: 'When a person owns something we use apostrophe. Like a child\'s toy.'
+          }
+        ]
+      }
+    }
+  }, {
+    id: 'largest-timed-mcq',
+    kind: 'multipleChoiceResponse',
+    time: 1,
+    isTimed: 20,
+    maxSelected: 1,
+    presenterImagePath: '/instructors/kunal2.jpg',
+    title: 'Fill In The Blank',
+    instructions: dedent`
+      The opposite of largest is ______.
+    `,
+    choices: [
+      {
+        id: 'opt1',
+        text: 'tallest'
+      },
+      {
+        id: 'opt2',
+        text: 'fittest'
+      },
+      {
+        id: 'opt3',
+        text: 'fastest'
+      },
+      {
+        id: 'opt4',
+        text: 'smallest'
+      }
+    ],
+    automaticResponses: [
+      {
+        answerSet: ['opt1',],
+        response: '__Wrong__. Tallest means सबसे लम्बा. Largest is सबसे बड़ा या सबसे ज़्यादा.'
+      },
+      {
+        answerSet: ['opt2'],
+        response: '__Incorrect__. To be fit means someone in good health (तंदरुस्त). Fittest means the most fit person (सबसे तंदरुस्त). Largest is सबसे बड़ा या सबसे ज़्यादा.'
+      },
+      {
+        answerSet: ['opt3'],
+        response: '__Wrong__. Fast means someone that moves at high speed (तेज़). Fastest means the person who moves most quickly (सबसे तेज़). Largest is सबसे बड़ा या सबसे ज़्यादा.'
+      },
+      {
+        answerSet: ['opt4'],
+        response: '__Correct__. Small means something tiny (छोटी चीज़). Smallest means the tiniest thing (सबसे छोटी चीज़). Largest is सबसे बड़ा या सबसे ज़्यादा.'
+      }
+    ]
+  }, ]
+}, {
   id: 'maggi',
   thumbnailImagePath: '/chapter-thumbnails/english/maggi.jpeg',
   title: 'Maggi',
